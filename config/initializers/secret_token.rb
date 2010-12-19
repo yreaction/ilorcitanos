f2cf4c9741c04d03c4900ea3ec738ec12f0ba77f
yreaction <Yreaction@iMac-de-yreaction.local>
2010-12-19 20:40:35 +0100

    Post inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ilorcitanos::Application.config.secret_token = 'e7b8d98e129c61d5ab1e31f06bcb49ba075f2c38a83b5beb60482d42af87d4120b801a2e6c076e8844471f627435412eeb515dd64796d5b7cc2cb15a37bca8d6'
